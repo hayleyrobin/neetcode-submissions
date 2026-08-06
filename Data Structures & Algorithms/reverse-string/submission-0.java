7be2c073d562b42c0ae48e70721dcc3f2c2a48d7
hayleyrobin <52174665+hayleyrobin@users.noreply.github.com>
2026-08-05 21:41:33 -0400

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.java
@@ -0,0 +1,33 @@
+/*
+Understand
+    Input: arr of chars, representing a string
+    Output: void, in-place reverse chars in arr
+    Case: ['t','a','n'] -> ['n', 'a', 't']
+        
+Match
+    Two Pointers -> in place modifications
+        Left ptr = left side of Array
+        Right ptr = right side of Array 
+Plan
+    Initialize ptrs at 0 and s.length -1
+    while(left < right)
+        swap chars
+        move pointers inwards
+Review/Evaluate
+    Time: O(n) -> traverse entire Array
+    Space: O(1) -> 2 pointers , no extra space 
+*/
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while(left <= right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
